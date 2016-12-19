faf742ff9d25bb9576b611665a171e7323cfc664
Nicolas Peña Escarpentier <npescarpentier@gmail.com>
2016-12-19 19:47:12 -0300

    Add logo, intro and playlist

--- mascapp.js
+++ mascapp.js
@@ -3,3 +3,8 @@ var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
+
+
+var client_id = ''; // Your client id
+var client_secret = ''; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
